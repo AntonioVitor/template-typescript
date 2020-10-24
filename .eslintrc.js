7e98f94e2ca01130e0356e574fc510b2caa3e1dd
Antonio Vitor <antoniovitorFBN@gmail.com>
2020-10-23 23:40:30 -0300

    Adds eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+};
